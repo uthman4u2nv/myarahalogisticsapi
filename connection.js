5a833df11d4be27b5f72eb07dbcd191296179298
Ajagbe Uthman Olawale <uthman4u2nv@yahoo.com>
2023-02-20 15:08:02 +0100

    Modified the connection String

--- connection.js
+++ connection.js
@@ -5,24 +5,9 @@ const hostname=process.env.HOSTNAME;
 const username=process.env.USERNAME;
 const pwsd=process.env.PASSWORD;
 const db=process.env.DATABASE;
-//const dbport=process.env.PORT;
+const dbport=process.env.DBPORT;
 console.log(process.env.HOSTNAME);
-/*const mysqlConnection = mysql.createPool({
-    connectionLimit : 1000, //important
-    connectTimeout  : 60 * 60 * 1000,
-    acquireTimeout  : 60 * 60 * 1000,
-    timeout         : 60 * 60 * 1000,
-    host:"web2.weblagos.com",
-    user:"qllpepqt_membership",
-    password:"2021@Usagbe",
-    database:"qllpepqt_membership",
-    port:3306
-    host:hostname,
-    user:username,
-    password:pwsd,
-    database:db,
-    port:3306
-});*/
+
 
 var pool = mysql.createPool({
     connectionLimit:1000,
@@ -30,7 +15,7 @@ var pool = mysql.createPool({
     user: username,
     password: pwsd,
     database:db,
-    port:8889
+    port:dbport
     
   });
   
